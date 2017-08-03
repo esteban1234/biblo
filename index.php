9b1d519554e5c5d94d60f7b913b706f9f82e92bf
esteban1234 <tva_libra@hotmail.com>
2017-08-03 18:13:11 -0500

    solo falta el formulario

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>BIBLO COMERCIALIZADORA DEL MAYAB</title>
+
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    
+
+    <!-- Scroll -->
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
+      selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+      speed: 2000, // Integer. How fast to complete the scroll in milliseconds
+      easing: 'easeInOutCubic', // Easing pattern to use
+      offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+      callback: function ( anchor, toggle ) {} // Function to run after scrolling
+      });
+    </script>
+    
+  </head>
+  <body>
+
+
+  <header id="sec-0">
+    <figure class="logo pull-left">
+      <img src="img/biblo.png" class="img-responsive" alt="">
+    </figure>
+    <nav class="pull-right">
+      <ul>
+        <a class="selector" id="select" data-scroll href="#sec-0" aria-pressed="true">INICIO</a>
+        <a class="selector" data-scroll href="#sec-1" aria-pressed="true">NOSOTROS</a>
+        <a class="selector" data-scroll href="#sec-2" aria-pressed="true">SERVICIOS</a>
+        <a class="selector" data-scroll href="#sec-3" aria-pressed="true">CONTACTO</a>
+      </ul>
+    </nav>
+  </header>
+
+<section class="seccion1" id="sec-1">
+  <div class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center text-danger">Acerca de nosotros</h1> <br><br><br>
+          <p class="lead text-center">Somos el distribuidor y mayorista líder en el país con un amplio portafolio de productos de ferreteria, tlapaleria, partes y refacciones nuevas para automóviles,
+          camionetas y camiones. </p>
+        </div>
+      </div> <br><br><br>
+
+
+        <div class="row">
+          <div class="col-md-4">
+            <!-- <img src="img/1.jpg" class="img-responsive"> -->
+            <div class="iconos">
+              <i class="fa fa-check-square-o fa-3x fa-border text-danger" aria-hidden="true"></i>
+              <!-- <i class="fa fa-address-book-o" aria-hidden="true"></i> -->
+            </div>
+            <h2 class="text-center">MISION</h2>
+            <p class="text-center">Nos especializamos en la comercialización de productos para ferreterías y tlapalerías, partes y refacciones nuevas para automóviles, camionetas y camiones, llegando a los principales canales de distribución, presentando excelentes servicios de venta y pos-venta a nuestros clientes y proporcionando satisfactorios resultados de crecimiento por la distribución de productos a nuestros proveedores.</p>
+          </div>
+          <div class="col-md-4">
+            <div class="iconos">
+              <i class="fa fa-line-chart fa-3x fa-border text-danger" aria-hidden="true"></i>
+              <!-- <i class="fa fa-address-book-o" aria-hidden="true"></i> -->
+            </div>
+            <h2 class="text-center">VISION</h2>
+            <p class="text-center">Ser la comercializadora con más amplia cobertura en servicio y tecnología de productos para ferreterías y tlapalerías, partes y refacciones nuevas para automóviles, camionetas y camiones. contando con el más eficiente y competente personal, brindando una atención con calidad y responsabilidad.</p>
+          </div>
+          <div class="col-md-4">
+            <div class="iconos">
+              <i class="fa fa-handshake-o fa-3x fa-border text-danger" aria-hidden="true"></i>
+              <!-- <i class="fa fa-address-book-o" aria-hidden="true"></i> -->
+            </div>
+            <h2 class="text-center">VALORES</h2>
+            <p class="text-center">
+            * Trabajo en equipo <br>
+              * Responsabilidad <br>
+              * Honestidad <br>
+              * Eficiencia <br>
+              * Respeto <br>
+              * Calidad
+            </p>
+          </div>
+        </div>
+
+
+    </div>
+  </div>
+  <div style="text-align: center; margin-top: 5%;">
+      <a class="btn btn-danger btn-outline btn-lg" data-scroll href="#sec-3">¡CONTACTANOS!</a>
+  </div>
+</section>
+
+<section class="seccion2" id="sec-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center text-danger">SERVICIOS</h1> <br><br><br>
+          <!-- <p class="lead text-center">Para que tu operación nunca se detenga, BIBLO te ofrece soluciones que te permitirán contar con los productos que necesitas cuando y donde los necesitas.</p> -->
+        </div>
+      </div>  <br><br><br>
+       <div class="row">
+          <div class="col-md-4">
+            <div class="imagen">
+              <img src="img/ferre.png" class="img-responsive">
+            </div>
+            <h3 class="text-center">Comercio de articulos para ferreterías</h3>
+            <!-- <p class="text-center">Nos especializamos en la comercialización de productos para ferreterías y tlapalerías, partes y refacciones nuevas para automóviles, camionetas y camiones, llegando a los principales canales de distribución, presentando excelentes servicios de venta y pos-venta a nuestros clientes y proporcionando satisfactorios resultados de crecimiento por la distribución de productos a nuestros proveedores.</p> -->
+          </div>
+          <div class="col-md-4">
+           <div class="imagen">
+              <img src="img/tlapa.png" class="img-responsive">
+            </div>
+            <h3 class="text-center">Comercio de articulos para tlapalerías</h3>
+            <!-- <p class="text-center">Ser la comercializadora con más amplia cobertura en servicio y tecnología de productos para ferreterías y tlapalerías, partes y refacciones nuevas para automóviles, camionetas y camiones. contando con el más eficiente y competente personal, brindando una atención con calidad y responsabilidad.</p> -->
+          </div>
+          <div class="col-md-4">
+            <div class="imagen">
+              <img src="img/carro.png" class="img-responsive">
+            </div>
+            <h3 class="text-center">Comercio de partes y refacciones para automoviles</h3>
+            <!-- <p class="text-center">
+            * Trabajo en equipo <br>
+              * Responsabilidad <br>
+              * Honestidad <br>
+              * Eficiencia <br>
+              * Respeto <br>
+              * Calidad
+            </p> -->
+          </div>
+        </div>
+
+        <br><br><br>
+
+       <div class="row">
+          <div class="col-md-4 col-md-offset-2">
+            <div class="imagen">
+              <img src="img/camion.png" class="img-responsive">
+            </div>
+            <h3 class="text-center">Comercio de partes y refacciones para camiones</h3>
+            <!-- <p class="text-center">Nos especializamos en la comercialización de productos para ferreterías y tlapalerías, partes y refacciones nuevas para automóviles, camionetas y camiones, llegando a los principales canales de distribución, presentando excelentes servicios de venta y pos-venta a nuestros clientes y proporcionando satisfactorios resultados de crecimiento por la distribución de productos a nuestros proveedores.</p> -->
+          </div>
+          <div class="col-md-4">
+           <div class="imagen">
+              <img src="img/van.png" class="img-responsive">
+            </div>
+            <h3 class="text-center">Comercio de partes y refacciones para camionetas</h3>
+            <!-- <p class="text-center">Ser la comercializadora con más amplia cobertura en servicio y tecnología de productos para ferreterías y tlapalerías, partes y refacciones nuevas para automóviles, camionetas y camiones. contando con el más eficiente y competente personal, brindando una atención con calidad y responsabilidad.</p> -->
+          </div>
+      
+        </div>
+    </div>
+</section>
+
+<section class="seccion1" id="sec-3">
+  <div class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center text-danger">CONTACTO</h1> <br><br><br>
+          <p class="lead text-center">Encuentranos cerca de ti, teléfonos, horarios, direcciones y contacto.</p>
+        </div>
+      </div> <br><br><br>
+      <div class="mapa">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1862.2788528992846!2d-89.62061164207454!3d21.0103594965021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5676a5f3c608e1%3A0xbe8c315b3b0d4e60!2sCalle+7A+279E%2C+Campestre%2C+97120+M%C3%A9rida%2C+Yuc.!5e0!3m2!1ses-419!2smx!4v1501800826200" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> <br><br><br>
+      </div>
+
+        <div class="row">
+          <div class="col-md-3">
+            <!-- <img src="img/1.jpg" class="img-responsive"> -->
+            <div class="iconos">
+              <i class="fa fa-clock-o fa-3x fa-border text-danger" aria-hidden="true"></i>
+              <!-- <i class="fa fa-address-book-o" aria-hidden="true"></i> -->
+            </div>
+            <h2 class="text-center">Horario</h2>
+            <p class="text-center">Lunes a Viernes de 9 am a 6 pm</p>
+          </div>
+          <div class="col-md-3">
+            <div class="iconos">
+              <i class="fa fa-envelope-o fa-3x fa-border text-danger" aria-hidden="true"></i>
+              <!-- <i class="fa fa-address-book-o" aria-hidden="true"></i> -->
+            </div>
+            <h2 class="text-center">Correo</h2>
+            <p class="text-center">info@biblocomercializadora.com</p>
+          </div>
+          <div class="col-md-3">
+            <div class="iconos">
+              <i class="fa fa-phone fa-3x fa-border text-danger" aria-hidden="true"></i>
+              <!-- <i class="fa fa-address-book-o" aria-hidden="true"></i> -->
+            </div>
+            <h2 class="text-center">Teléfono</h2>
+            <p class="text-center">Pendiente
+            </p>
+          </div>
+
+          <div class="col-md-3">
+            <div class="iconos">
+              <i class="fa fa-map-marker fa-3x fa-border text-danger" aria-hidden="true"></i>
+              <!-- <i class="fa fa-address-book-o" aria-hidden="true"></i> -->
+            </div>
+            <h2 class="text-center">Dirección</h2>
+            <p class="text-center">C. 7-A #279 E por C. 40 y C. 38 Col. Campestre, Mérida, Yucatán. C.P.97120.
+            </p>
+          </div>
+        </div>
+
+
+    </div>
+  </div>
+</section>
+
+<footer>
+<p>Derechos de autor 2017 © | BIBLO COMERCIALIZADORA DEL MAYAB S. de R.L de C.V. </p>
+</footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+  </body>
+</html>
